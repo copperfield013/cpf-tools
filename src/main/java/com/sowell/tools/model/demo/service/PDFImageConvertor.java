8b9fad2c2d929bc42c13ff34ee92704f1f472a34
copperfield <copperfield@6026f5bf-e984-4331-9919-80eb141a14a1>
2017-11-03 07:51:55 +0000

    git-svn-id: http://47.100.187.235:13932/svn/cpf-tools@10 6026f5bf-e984-4331-9919-80eb141a14a1

--- src/main/java/com/sowell/tools/model/demo/service/PDFImageConvertor.java
+++ src/main/java/com/sowell/tools/model/demo/service/PDFImageConvertor.java
@@ -1,6 +1,9 @@
 package com.sowell.tools.model.demo.service;
 
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -13,6 +16,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.tools.imageio.ImageIOUtil;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.Assert;
 
 import com.sowell.tools.util.ProgressRecorder;
@@ -24,7 +28,44 @@ public class PDFImageConvertor {
 	private PDFRenderer pdfRenderer;
 	private ProgressRecorder progressRecorder;
 	
-	Logger logger = Logger.getLogger(PDFImageConvertor.class);
+	static Logger logger = Logger.getLogger(PDFImageConvertor.class);
+	
+	static{
+		logger.debug("=============初始化PDF图像转换器========================");
+		GraphicsEnvironment ge = 
+				GraphicsEnvironment.getLocalGraphicsEnvironment();
+		String[] fontFamilies = ge.getAvailableFontFamilyNames();
+		logger.debug("+++++所有支持字体++++++");
+		for (String s : fontFamilies) {
+			logger.debug(s);
+		}
+		ClassPathResource resource = new ClassPathResource("font/");
+		try {
+			File folder = resource.getFile();
+			logger.debug("加载字体文件目录：" + folder.getAbsolutePath());
+			File[] files = folder.listFiles((dir, name)->{
+				logger.debug("加载字体文件:" + name);
+				if(name.length() > 4){
+					String suffix = name.substring(name.length() - 4, name.length());
+					return suffix.equalsIgnoreCase(".ttf");
+				}
+				return false;
+			});
+			if(files != null && files.length > 0){
+				for (File file : files) {
+					try {
+						ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, file));
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} catch (IOException e1) {
+			logger.error("", e1);
+		}
+		logger.debug("=============初始化PDF图像转换器结束========================");
+	}
+	
 	
 	public PDFImageConvertor(InputStream inputStream, int dpi, int[] converteIndex) throws IOException{
 		this.document = PDDocument.load(inputStream);
@@ -48,6 +89,7 @@ public class PDFImageConvertor {
 		Assert.notNull(outputs);
 		Assert.isTrue(outputs.length == indexs.length);
 		for (int i = 0; i < indexs.length; i++) {
+			
 			BufferedImage bim = pdfRenderer.renderImageWithDPI(indexs[i], this.dpi,
 					ImageType.RGB);
 			this.progressRecorder.setProgressMsg("正在转换第" + (indexs[i] + 1) + "页，当前进度" + (i + 1) + "/" + indexs.length + "页").incStep();
